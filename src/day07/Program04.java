d80e43473617afdd5ff67458266f681b11ee921c
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-02-13 08:57:23 +0530

    day07

---
+++ src/day07/Program04.java
@@ -0,0 +1,32 @@
+package day07;
+
+
+public class Program04 {
+
+	public static void main(String[] args) {
+
+		int a=100;
+		int b=100;
+
+		System.out.println("Start Program...!");
+
+		if(a>b)
+		{
+			System.out.println("a is greater..!");
+		}
+		else if(a<b)
+		{
+			System.out.println("b is greater..!");
+		}
+		else
+		{
+			System.out.println("a & b are equal..!");
+		}
+
+		System.out.println("End Program...!");
+
+	
+		
+	}
+
+}
