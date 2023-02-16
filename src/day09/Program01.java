7e349d23a83052be430b92bc7753e222d9ab71bc
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-02-16 09:00:54 +0530

    day09

---
+++ src/day09/Program01.java
@@ -0,0 +1,33 @@
+package day09;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		System.out.println("Start Program..!");
+		//1-5
+		int a=1;
+		while(a<=5)//1<=5 2<=5 3<=5 4<=5 5<=5
+		{
+		System.out.println("Good Day..!");//1 2 3 4 5
+		a=a+1;//2 3 4 5 6
+		}
+		
+		double b=0.5;
+		while(b>=0.1)
+		{
+			System.out.println("Good Morning..!");
+			b=b-0.1;
+		}
+		
+		int c=-11;
+		while(c>=-15)
+		{
+			System.out.println("@@@@@@");
+			c=c-1;
+		}
+		
+		System.out.println("End Program..!");
+
+	}
+
+}
