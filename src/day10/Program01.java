6f0a5959e59e57774626bc2ca5ccc3e901282245
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-02-17 08:20:11 +0530

    Day10

---
+++ src/day10/Program01.java
@@ -0,0 +1,25 @@
+package day10;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+
+		int a=1;
+		do
+		{
+			System.out.println("Good Day..!");//1 2 3 4 5
+			a=a+1;
+		}while(a<=5);//2 3 4 5 6
+		System.out.println("$$$$$$$$$$$$$$$$$$$");
+
+		int b=5;
+		do
+		{
+			System.out.println("Good Day..!");
+			b=b-1;
+		}while(b>=1);
+
+
+	}
+
+}
