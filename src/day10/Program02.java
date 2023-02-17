ebd82706d2e05a4717dc73b30ae0fa59ccdbc1de
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-02-17 08:29:02 +0530

    for Loop

---
+++ src/day10/Program02.java
@@ -0,0 +1,19 @@
+package day10;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		
+		for(int a=1;a<=5;a=a+1)
+		{
+			System.out.println("Good Day..!");
+		}
+		
+		for(int b=-1;b>=-5;b=b-1)
+		{
+			System.out.println("Good Morning..!");
+		}
+
+	}
+
+}
